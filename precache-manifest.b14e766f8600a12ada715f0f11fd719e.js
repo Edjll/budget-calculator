d90f0e04853d4992a46a13c8e847b5d5bc4f4866
Vlad Shevyakov <v.shevy@ya.ru>
2020-05-19 22:29:07 +0400

    Updates

---
+++ precache-manifest.b14e766f8600a12ada715f0f11fd719e.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5138fb204d50a3179510bc1eb5ac47ca",
+    "url": "/budget-calculator/index.html"
+  },
+  {
+    "revision": "1373f028280456330d4c",
+    "url": "/budget-calculator/static/css/main.afd3fa09.chunk.css"
+  },
+  {
+    "revision": "30af9b8888893e6d584a",
+    "url": "/budget-calculator/static/js/2.85dceec5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/budget-calculator/static/js/2.85dceec5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1373f028280456330d4c",
+    "url": "/budget-calculator/static/js/main.aa338ac4.chunk.js"
+  },
+  {
+    "revision": "3bc2d86cb6a369c382b5",
+    "url": "/budget-calculator/static/js/runtime-main.c986d838.js"
+  },
+  {
+    "revision": "d0eb6fd950fb08030b586e0ce1a4d2a3",
+    "url": "/budget-calculator/static/media/calculator.d0eb6fd9.svg"
+  },
+  {
+    "revision": "458441162e64368eafcef890e81f3eb0",
+    "url": "/budget-calculator/static/media/delete.45844116.svg"
+  },
+  {
+    "revision": "b26a118cc9afded150f2f8200c84364f",
+    "url": "/budget-calculator/static/media/edit.b26a118c.svg"
+  },
+  {
+    "revision": "80f373595ee7095fd0c2ffb8fbff406b",
+    "url": "/budget-calculator/static/media/login.80f37359.svg"
+  },
+  {
+    "revision": "b810fd509ffb3275f525277b86489260",
+    "url": "/budget-calculator/static/media/password.b810fd50.svg"
+  },
+  {
+    "revision": "4d49b1eec686ee3b1e1f32737cbd1991",
+    "url": "/budget-calculator/static/media/refresh.4d49b1ee.svg"
+  },
+  {
+    "revision": "2d9cb43411451760fbb5c35fda702362",
+    "url": "/budget-calculator/static/media/registration.2d9cb434.svg"
+  },
+  {
+    "revision": "d82c894957c2380272df67c3d5d44b94",
+    "url": "/budget-calculator/static/media/save.d82c8949.svg"
+  },
+  {
+    "revision": "4b4752d64c278e4f47be92eac431b15a",
+    "url": "/budget-calculator/static/media/settings.4b4752d6.svg"
+  },
+  {
+    "revision": "b6fc0f159666b3e0757e3929421892bd",
+    "url": "/budget-calculator/static/media/username.b6fc0f15.svg"
+  }
+]);

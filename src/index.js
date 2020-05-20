f0c1d897ab684558f2ac3a9eb4f66e3a90ab7d44
Vlad Shevyakov <v.shevy@ya.ru>
2020-05-20 16:08:07 +0400

    delete second calculator

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import * as serviceWorker from './serviceWorker';
 ReactDOM.render(
   <React.StrictMode>
     <BudgetCalculator />
-    <BudgetCalculator />
   </React.StrictMode>,
   document.getElementById('root')
 );
